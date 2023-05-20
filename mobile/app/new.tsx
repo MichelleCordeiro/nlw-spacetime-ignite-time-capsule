801d46087439ac9bbda6b0a0f309233580544759
MichelleCordeiro <mic321@gmail.com>
2023-05-20 15:34:54 -0300

    14: mobile - layout route, customized initial route and new memories page

---
+++ mobile/app/new.tsx
@@ -0,0 +1,81 @@
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Switch,
+  TextInput,
+} from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
+
+import NLWLogoH from '../src/assets/nlw-spacetime-logo-h.svg'
+import { Link } from 'expo-router'
+import React, { useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+  // C useSafeAreaInsets é possível saber o tamanho da statusbar e do footer do IOS(nut)
+  // No android a statusbar tem 20 e no IOS 40
+  // C isso dá p saber a distância certa p nenhum elemento ficar escondido neles
+
+  const [isPublic, setIsPublic] = useState(false)
+
+  return (
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogoH />
+
+        <Link href="/memories" asChild>
+          {/* C o asChild o TouchableOpacity se comportará como um Link */}
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+            <Icon name="arrow-left" size={16} color="#FFF" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+
+      <View className="mt-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch
+            value={isPublic}
+            onValueChange={setIsPublic}
+            trackColor={{ false: '#767577', true: '#4d3486' }}
+            thumbColor={isPublic ? '#9b79ea' : '#b0b0b0'}
+          />
+          <Text className="font-body text-base text-gray-200">
+            Tornar memória pública
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
+          <View className="flex-row items-center gap-2">
+            <Icon name="image" color={'#FFF'} />
+            <Text className="font-body text-sm text-gray-200">
+              Adicionar foto ou vídeo de capa
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TextInput
+          multiline
+          className="p-0 font-body text-lg text-gray-50"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+          placeholderTextColor={'#56565a'}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="items-center self-end rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  )
+}
