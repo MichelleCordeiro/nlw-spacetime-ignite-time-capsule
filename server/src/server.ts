3ae5aa75ee0e3880f2444b73f93ee0efcea89a75
MichelleCordeiro <mic321@gmail.com>
2023-05-20 21:59:07 -0300

    15: server - files upload route and serving statics files

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,22 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+// ou sem o import
+// app.register(require('@fastify/multipart'))
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas as urls de frontend poderam acessar no backend
@@ -18,6 +30,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
